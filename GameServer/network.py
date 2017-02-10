cad11a9e8f22bb503431ff195f93ce79a5cdaba5
Juan Gonzalez <jgonzalez498@gmail.com>
2017-02-09 17:26:44 -0800

    Accepts WebSocket requests, processes handshakes, parses and reads incoming messages.

--- GameServer/network.py
+++ GameServer/network.py
@@ -1,4 +1,10 @@
+import socket as sock
 import pymysql
+import hashlib
+import base64
+import re
+import binascii
+
 from pymysql.cursors import DictCursor
 from logger import Logger
 from enum import IntEnum
@@ -64,7 +70,7 @@ class Request:
 # Helper class that contains useful network functions
 class Network:
 
-    # Translates an int to 3 bytes - returns bytearray object
+    # Translates an int to 3 bytes - returns byte array object
     @staticmethod
     def int_3byte(num):
 
@@ -76,25 +82,20 @@ class Network:
 
         return _3byte
 
+    # Determines if the connection is from a WebSocket
     @staticmethod
-    def parse_request(client, data):
+    def check_wconnection(client):
 
-        Logger.log("Raw Request: " + str(data))
+        is_websocket = False
 
-        flag = data[0]
-        token = data[1:25].decode('utf-8')
-        size = int.from_bytes(data[25:28], byteorder='big')
+        # Peek to get data without pulling from queue
+        data = (client.recv(1024, sock.MSG_PEEK)).decode('utf-8')
 
-        body = None
-        if size > 0:
-            body = Network.receive_data(client, size)
-            Logger.log("Raw Body: " + str(body))
+        # If the request is a HTTP GET flush it from the queue and initiate handshake
+        if re.search('GET', data):
+            is_websocket = True
 
-        if body:
-            body = body.decode('utf-8')
-
-        request = Request(flag, token, size, body)
-        return request
+        return is_websocket
 
     # Creates response header with flag and size
     @staticmethod
@@ -177,3 +178,84 @@ class Network:
             client.sendall(data)
         except:
             pass
+
+    @staticmethod
+    def parse_request(client, data):
+
+        Logger.log("Raw Request: " + str(binascii.hexlify(data)))
+
+        flag = data[0]
+        token = data[1:25].decode('utf-8')
+        size = int.from_bytes(data[25:28], byteorder='big')
+
+        body = None
+        if size > 0:
+            body = Network.receive_data(client, size)
+            Logger.log("Raw Body: " + str(body))
+
+        if body:
+            body = body.decode('utf-8')
+
+        request = Request(flag, token, size, body)
+        return request
+
+
+# Helper class that contains useful WebSocket related network functions
+class WNetwork(Network):
+
+    @staticmethod
+    def handshake(client):
+
+        request = (client.recv(1024)).decode('utf-8')
+
+        response = \
+            b'HTTP/1.1 101 Switching Protocols\r\n' \
+            b'Upgrade: websocket\r\n' \
+            b'Connection: Upgrade\r\n' \
+            b'Sec-WebSocket-Accept: %s\r\n\r\n'
+
+        magic_key = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
+
+        match = re.search('Sec-WebSocket-Key: (.*)\r\n', request)
+        if match:
+
+            accept_key = (match.group(1) + magic_key).encode('utf-8')
+            accept_key = hashlib.sha1(accept_key).digest()
+            accept_key = base64.b64encode(accept_key)
+
+            client.sendall(response % accept_key)
+
+    @staticmethod
+    def parse_request(client, data):
+
+        payload = WNetwork.read_frame(client, data)
+        Logger.log("Payload: " + str(payload))
+
+    @staticmethod
+    def read_frame(client, data):
+
+        decoded_payload = bytearray()
+        while True:
+
+            fin = (data[0] & 0x80) >> 7
+            # opcode = (data[0] & 0x0F)
+            # mask = (data[1] & 0x80) >> 7
+            payload_len = (data[1] & 0x7F)
+
+            if payload_len == 126:
+                payload_len = WNetwork.receive_data(client, 2)  # or 0
+            elif payload_len == 127:
+                payload_len = WNetwork.receive_data(client, 8)  # or 0
+
+            mask_key = WNetwork.receive_data(client, 4)  # or 0
+            payload = WNetwork.receive_data(client, payload_len)
+
+            for index in range(len(payload)):
+                decoded_payload.append(payload[index] ^ mask_key[index % 4])
+
+            if fin == 1:
+                break
+
+            data = WNetwork.receive_data(client, 2)
+
+        return decoded_payload
